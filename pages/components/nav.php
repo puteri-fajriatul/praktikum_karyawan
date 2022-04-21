968bf8918e84c998bd4073ce634a58ddc0257fb4
puterifajriatulhasanah <puteri.hasanah01@gmail.com>
2022-04-21 09:42:26 +0700

    Pemecahan Komponen

---
+++ pages/components/nav.php
@@ -0,0 +1,30 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="?page=dashboard">
+              <span data-feather="home"></span>
+              Dashboard
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="?page=karyawan">
+              <span data-feather="users"></span>
+              Data Karyawan
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="?page=bagian">
+              <span data-feather="layers"></span>
+              Data Bagian
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="rewind"></span>
+             Logout
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
