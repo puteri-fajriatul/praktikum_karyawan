81737e3b3c7b92c41d556125a1570b0deee3e86b
puterifajriatulhasanah <puteri.hasanah01@gmail.com>
2022-04-21 09:59:35 +0700

    Menambahkan Card Pada Dashboard

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -2,7 +2,24 @@
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
-        </div>
       </div>
+</div>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="card bg-primary" style="height: 9rem"></div>
+    </div>
+      <div class="col">
+        <div class="card bg-success" style="height: 9rem"></div>
+    </div>
+      <div class="col">
+        <div class="card bg-warning" style="height: 9rem"></div>
+    </div>
+      <div class="col">
+        <div class="card bg-danger" style="height: 9rem"></div>
+    </div>
   </div>
+</div>
+</div>
+</div>
 </main>
